199dc29adfbb3fc61c54d95663d8d57de3e6270f
EdwardDNB <edvarddnbs@gmail.com>
2022-09-07 20:51:48 +0300

    3

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ const server = http.createServer((request, response) => {
     switch (request.url) {
         case'/home': {
             const start = new Date()
-            while (new Date(5000) - start < 5000) {
-                console.log(new Date() - start < 5000)
+            while (new Date(3000) - start < 3000) {
+                console.log(new Date() - start < 3000)
             }
             const data = 'best course'
             response.write(data)
-            response.end()
             break
         }
         default: {
             response.write('404 not found')
-            response.end()
+
         }
     }
-
+    response.end()
 
 })
 
